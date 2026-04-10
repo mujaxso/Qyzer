3130d1f6e2707af7bddafc1e4a2d04b63a5275da
Mujahid Siyam <mujaxso@proton.me>
2026-04-10 21:56:18 +0200

    chore: remove debug print statements from desktop app

--- apps/desktop/src/commands.rs
+++ apps/desktop/src/commands.rs
@@ -14,15 +14,12 @@ impl Command {
     pub fn execute(&self) {
         match &self.command {
             EditorCommand::OpenWorkspace { path } => {
-                println!("Opening workspace at: {}", path);
                 // TODO: Actually open the workspace
             }
             EditorCommand::OpenFile { path } => {
-                println!("Opening file: {}", path);
                 // TODO: Actually open the file
             }
             EditorCommand::SaveActiveFile => {
-                println!("Saving active file");
                 // TODO: Actually save the file
             }
         }
