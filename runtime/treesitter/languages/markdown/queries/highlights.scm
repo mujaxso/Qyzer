8a1dd0c39db89465571ac6a97c269cc03e64d31b
Mujahid Siyam <mujaxso@proton.me>
2026-04-15 22:17:48 +0200

    feat: add markdown highlighting query with known node types

--- runtime/treesitter/languages/markdown/queries/highlights.scm
+++ runtime/treesitter/languages/markdown/queries/highlights.scm
@@ -1,3 +1,31 @@
-; Minimal markdown highlighting query
-; Using only wildcard pattern that will compile with any grammar
+; Markdown highlighting query for tree-sitter-markdown-inline
+; Based on actual node types from the grammar
+
+; Try to match various inline elements we saw in debug output
+(_backslash_escape) @string.escape
+(entity_reference) @string
+(numeric_character_reference) @string
+
+; Try to match punctuation
+[
+  "["
+  "]"
+  "<"
+  ">"
+  "!"
+  "\""
+  "#"
+  "$"
+  "%"
+  "&"
+  "'"
+  "*"
+  "+"
+  ","
+  "-"
+  "."
+] @operator
+
+; Try to match text content (if such a node exists)
+; We'll use wildcard for everything else
 (_) @plain
