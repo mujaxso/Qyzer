a661779465d274a16fe4297711a22a6038d228bc
Mujahid Siyam <mujaxso@proton.me>
2026-04-15 11:30:03 +0200

    feat: improve Rust treesitter highlighting queries

--- runtime/treesitter/languages/rust/queries/highlights.scm
+++ runtime/treesitter/languages/rust/queries/highlights.scm
@@ -1,43 +1,131 @@
-; Rust highlight queries for Tree‑sitter (compatible with tree‑sitter‑rust 0.20)
+; Identifiers
 
-; Comments
+(type_identifier) @type
+(primitive_type) @type.builtin
+(field_identifier) @property
+
+; Identifier conventions
+
+; Assume all-caps names are constants
+((identifier) @constant
+ (#match? @constant "^[A-Z][A-Z\\d_]+$'"))
+
+; Assume uppercase names are enum constructors
+((identifier) @constructor
+ (#match? @constructor "^[A-Z]"))
+
+; Assume that uppercase names in paths are types
+((scoped_identifier
+  path: (identifier) @type)
+ (#match? @type "^[A-Z]"))
+((scoped_identifier
+  path: (scoped_identifier
+    name: (identifier) @type))
+ (#match? @type "^[A-Z]"))
+((scoped_type_identifier
+  path: (identifier) @type)
+ (#match? @type "^[A-Z]"))
+((scoped_type_identifier
+  path: (scoped_identifier
+    name: (identifier) @type))
+ (#match? @type "^[A-Z]"))
+
+; Assume all qualified names in struct patterns are enum constructors. (They're
+; either that, or struct names; highlighting both as constructors seems to be
+; the less glaring choice of error, visually.)
+(struct_pattern
+  type: (scoped_type_identifier
+    name: (type_identifier) @constructor))
+
+; Function calls
+
+(call_expression
+  function: (identifier) @function)
+(call_expression
+  function: (field_expression
+    field: (field_identifier) @function.method))
+(call_expression
+  function: (scoped_identifier
+    "::"
+    name: (identifier) @function))
+
+(generic_function
+  function: (identifier) @function)
+(generic_function
+  function: (scoped_identifier
+    name: (identifier) @function))
+(generic_function
+  function: (field_expression
+    field: (field_identifier) @function.method))
+
+(macro_invocation
+  macro: (identifier) @function.macro
+  "!" @function.macro)
+
+; Function definitions
+
+(function_item (identifier) @function)
+(function_signature_item (identifier) @function)
+
 (line_comment) @comment
 (block_comment) @comment
 
-; Strings
-(string_literal) @string
-(raw_string_literal) @string
-(char_literal) @string
+(line_comment (doc_comment)) @comment.documentation
+(block_comment (doc_comment)) @comment.documentation
 
-; Keywords
+"(" @punctuation.bracket
+")" @punctuation.bracket
+"[" @punctuation.bracket
+"]" @punctuation.bracket
+"{" @punctuation.bracket
+"}" @punctuation.bracket
+
+(type_arguments
+  "<" @punctuation.bracket
+  ">" @punctuation.bracket)
+(type_parameters
+  "<" @punctuation.bracket
+  ">" @punctuation.bracket)
+
+"::" @punctuation.delimiter
+":" @punctuation.delimiter
+"." @punctuation.delimiter
+"," @punctuation.delimiter
+";" @punctuation.delimiter
+
+(parameter (identifier) @variable.parameter)
+
+(lifetime (identifier) @label)
+
 "as" @keyword
 "async" @keyword
 "await" @keyword
 "break" @keyword
 "const" @keyword
 "continue" @keyword
-(crate) @keyword
+"default" @keyword
 "dyn" @keyword
 "else" @keyword
 "enum" @keyword
 "extern" @keyword
 "fn" @keyword
 "for" @keyword
+"gen" @keyword
 "if" @keyword
 "impl" @keyword
 "in" @keyword
 "let" @keyword
 "loop" @keyword
+"macro_rules!" @keyword
 "match" @keyword
 "mod" @keyword
 "move" @keyword
-(mutable_specifier) @keyword
 "pub" @keyword
+"raw" @keyword
 "ref" @keyword
 "return" @keyword
 "static" @keyword
 "struct" @keyword
-(super) @keyword
 "trait" @keyword
 "type" @keyword
 "union" @keyword
@@ -46,94 +134,28 @@
 "where" @keyword
 "while" @keyword
 "yield" @keyword
+(crate) @keyword
+(mutable_specifier) @keyword
+(use_list (self) @keyword)
+(scoped_use_list (self) @keyword)
+(scoped_identifier (self) @keyword)
+(super) @keyword
 
-; Function definitions
-(function_item (identifier) @function)
-(function_signature_item (identifier) @function)
-
-; Function calls
-(call_expression
-  function: (identifier) @function)
-(call_expression
-  function: (field_expression
-    field: (field_identifier) @function))
-
-; Type definitions
-(type_identifier) @type
-(primitive_type) @type
-
-; Variables
-(identifier) @variable
 (self) @variable.builtin
 
-; Lifetimes (common in Rust)
-(lifetime) @type
+(char_literal) @string
+(string_literal) @string
+(raw_string_literal) @string
 
-; Built-in types
-((type_identifier) @type.builtin
- (#match? @type.builtin "(i8|i16|i32|i64|i128|isize|u8|u16|u32|u64|u128|usize|f32|f64|bool|char|str|String|Vec|Option|Result|Box|Rc|Arc|RefCell|Cell)"))
+(boolean_literal) @constant.builtin
+(integer_literal) @constant.builtin
+(float_literal) @constant.builtin
 
-; Constants
-(const_item (identifier) @constant)
+(escape_sequence) @escape
 
-; Operators
-"+" @operator
-"-" @operator
-"*" @operator
-"/" @operator
-"%" @operator
-"=" @operator
-"==" @operator
-"!=" @operator
-"<" @operator
-"<=" @operator
-">" @operator
-">=" @operator
-"!" @operator
-"&&" @operator
-"||" @operator
-"&" @operator
-"|" @operator
-"^" @operator
-"<<" @operator
-">>" @operator
-"+=" @operator
-"-=" @operator
-"*=" @operator
-"/=" @operator
-"%=" @operator
-"&=" @operator
-"|=" @operator
-"^=" @operator
-"<<=" @operator
-">>=" @operator
-".." @operator
-"..=" @operator
-"->" @operator
-"=>" @operator
-
-; Punctuation
-"," @operator
-";" @operator
-":" @operator
-"::" @operator
-"." @operator
-"(" @operator
-")" @operator
-"[" @operator
-"]" @operator
-"{" @operator
-"}" @operator
-
-; Literals
-(boolean_literal) @constant
-(integer_literal) @constant
-(float_literal) @constant
-
-; Attributes
 (attribute_item) @attribute
 (inner_attribute_item) @attribute
 
-; Macros
-(macro_invocation
-  macro: (identifier) @function)
+"*" @operator
+"&" @operator
+"'" @operator
